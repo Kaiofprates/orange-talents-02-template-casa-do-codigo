4d48a58eb02fdd1f68b982f4f6d660f137600976
Kaiofprates <kaioprates@outlook.com>
2021-02-17 19:50:32 -0300

    create query all books

--- src/main/java/br/com/zup/desafio1/controllers/BookController.java
+++ src/main/java/br/com/zup/desafio1/controllers/BookController.java
@@ -1,6 +1,7 @@
 package br.com.zup.desafio1.controllers;
 
 import br.com.zup.desafio1.controllers.form.request.BookRequest;
+import br.com.zup.desafio1.controllers.form.response.BookFindAllRespose;
 import br.com.zup.desafio1.controllers.form.response.BookResponse;
 import br.com.zup.desafio1.models.Book;
 import br.com.zup.desafio1.models.Category;
@@ -10,8 +11,10 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 import javax.transaction.Transactional;
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/book")
@@ -22,9 +25,9 @@ public class BookController {
 
     @PostMapping
     @Transactional
-    public ResponseEntity<?> newBook(@RequestBody @Valid BookRequest request){
+    public ResponseEntity<?> newBook(@RequestBody @Valid BookRequest request) {
         Book book = request.toModel(manager);
-        if(book != null){
+        if (book != null) {
             manager.persist(book);
             return ResponseEntity.ok(new BookResponse(book).Response());
         }
@@ -32,14 +35,20 @@ public class BookController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<?> getBook(@PathVariable Long id){
-        Book book = manager.find(Book.class,id);
-        if(book != null ){
+    public ResponseEntity<?> getBook(@PathVariable Long id) {
+        Book book = manager.find(Book.class, id);
+        if (book != null) {
             return ResponseEntity.ok(new BookResponse(book).Response());
         }
         return ResponseEntity.notFound().build();
     }
 
+    @GetMapping
+    public ResponseEntity<?> findAll() {
+        Query query = manager.createQuery("SELECT id, title FROM Book");
+        List<BookFindAllRespose> response = query.getResultList();
+        return ResponseEntity.ok(response);
+    }
 
 
 }
---
+++ src/main/java/br/com/zup/desafio1/controllers/form/response/BookFindAllRespose.java
@@ -0,0 +1,19 @@
+package br.com.zup.desafio1.controllers.form.response;
+
+public class BookFindAllRespose {
+    private String title;
+    private String id;
+
+    public BookFindAllRespose(String title, String id) {
+        this.title = title;
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getId() {
+        return id;
+    }
+}
